1d3c54d315412e24cc735a3dde541a75336db25f
Arushi <arushi1467.cse18@chitkara.edu.in>
2021-01-31 23:19:18 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,16 @@
+var DATA=[];
+var showfav=false;
+
+//forms
+const questionForm=document.getElementById('question');
+const responseForm=document.getElementById('responseFornm');
+
+//buttons
+const newQuesBtn=document.getElementById('new-ques');
+const reolveBtn=document.getElementById('resolve');
+const responseButton=document.getElementByI('response-submit');
+const submitButton=document.getElementById('ques-submit');
+
+const welcome=document.getElementByI('welcome');
+const response=document.getElementById('response');
+const quesInfo=document.getElementById('ques-info');
